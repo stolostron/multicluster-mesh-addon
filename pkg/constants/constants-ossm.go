2aec327eb1e979b1a231a0ce5b40f4a4ca1098a2
Mike Kolesnik <mkolesni@redhat.com>
2026-02-24 14:44:46 +0200

    Reset repository for new implementation
    Remove all old code and replace with placeholder README.
    Preparing for new multicluster-mesh-addon architecture.
    
    Signed-off-by: Mike Kolesnik <mkolesni@redhat.com>

--- pkg/constants/constants-ossm.go
+++
@@ -1,7 +0,0 @@
-package constants
-
-// openshift service mesh constants
-const (
-	FederationEgressServiceLabelKey  = "federation.maistra.io/egress-for"
-	FederationIngressServiceLabelKey = "federation.maistra.io/ingress-for"
-)
