d2615b02cebac0e63b3a3f597e00ba0adfd5496b
Jacek Ewertowski <jacek.ewertowski1@gmail.com>
2026-05-20 22:38:16 +0200

    Fix `could remove embedded field "RawExtension" from selector (staticcheck)`
    Signed-off-by: Jacek Ewertowski <jacek.ewertowski1@gmail.com>

--- pkg/util/manifestwork.go
+++ pkg/util/manifestwork.go
@@ -9,5 +9,5 @@ import (
 // UnmarshalManifest extracts a manifest from ManifestWork's RawExtension.
 // When reading from the API, the Object field is nil and data is in Raw bytes.
 func UnmarshalManifest(manifest workv1.Manifest, into interface{}) error {
-	return json.Unmarshal(manifest.RawExtension.Raw, into)
+	return json.Unmarshal(manifest.Raw, into)
 }
