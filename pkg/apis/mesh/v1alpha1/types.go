3937e95018be528fdf5dbeb115f2aefa6dcb75ae
Yuanlin Xu <yuanlin.xu@redhat.com>
2026-06-16 08:11:30 -0400

    review updates
    Signed-off-by: Yuanlin Xu <yuanlin.xu@redhat.com>

--- pkg/apis/mesh/v1alpha1/types.go
+++ pkg/apis/mesh/v1alpha1/types.go
@@ -126,7 +126,6 @@ type DiscoveryConfig struct {
 	// Supports hours (h), minutes (m), seconds (s) or milliseconds (ms). If unset, defaults to 360h
 	// +optional
 	// +kubebuilder:default="360h"
-	// +kubebuilder:validation:XValidation:rule="matches(self, '^([0-9]{1,5}(h|m|s|ms)){1,4}$')",message="invalid duration value"
 	// +kubebuilder:validation:XValidation:rule="duration(self) >= duration('10m')", message="TokenValidity must be at least 10 minutes"
 	TokenValidity *metav1.Duration `json:"tokenValidity,omitempty"`
 }
